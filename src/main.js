60c50b73d882bb2cc16819758a89fc18b58dc52e
Ayyaz Zafar <ayyaz.zaffar@gmail.com>
2020-05-28 23:23:57 +0500

    improvement

--- src/main.js
+++ src/main.js
@@ -90,6 +90,8 @@ async function createGrphqlStructure(options) {
     await  replaceFileContent('src/Public/graphql/' + options.name+'/resolvers/query/index.ts', options.name);
     await  replaceFileContent('src/Public/graphql/' + options.name+'/resolvers/query/sample-query.resolver.ts', options.name);
 
+    await  replaceFileContent('src/Public/graphql/' + options.name+'/resolvers/subscription/sample.subscription.resolver.ts', options.name);
+
   });
 }
 
